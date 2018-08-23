6aa12ee2a5da3aa4363706ac1c119b632594b28e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 13:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ function BoardMember(name, homestate, training){
   this.name = name;
   this.homestate = homestate;
   this.training = training;
-  this.veto = function(){
-    "No, I must disagree"
-  }
+
 }
